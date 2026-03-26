108673e8531a8c51916d17928f2ae3d0c89ff6cd
Diyorbek Aslonov <aslonovdiyorbrk333@gmail.com>
2026-03-26 14:22:19 +0500

    add models, views, templates

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,26 +3,24 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
 
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
-
+        return super().get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published')
+    ('draft', 'Draft'),
+    ('published', 'Published')
     )
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique_for_date="publish")
+    title = models.CharField(max_length=225)
+    slug = models.SlugField(max_length=225, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES, default='draft')
+    created = models.DateTimeField(auto_now_add =True)
+    updated = models.DateTimeField(auto_now =True)
+    status = models.CharField(max_length =10, choices=STATUS_CHOICES)
 
     class Meta:
         ordering = ('-publish',)
@@ -35,14 +33,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.publish.year,
-                                                 self.publish.month,
-                                                 self.publish.day,
-                                                 self.slug])
+                                                          self.publish.month,
+                                                          self.publish.day,
+                                                          self.slug])
 
-
-posts = Post.objects.filter(status='published')
+posts = Post.objects.all()
 p_posts = Post.published.all()
-
-
 
 
--- blogapp/views.py
+++ blogapp/views.py
@@ -5,12 +5,11 @@ from django.views.generic import ListView
 
 
 class PostListView(ListView):
-    queryset = Post.published.all()
+    queryset =  Post.published.all()
     context_object_name = 'posts'
     paginate_by = 5
     template_name = "blog/post/list.html"
 
-
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status="published", publish__yead=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=slug, status="published, publish__year=year, publish__month=month, publish__day=day")
+    return render(request, 'blog/post/detail.html',{"post": post})
